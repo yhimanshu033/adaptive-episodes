ca242a227c9a44f0887f6c37b9bba9cdc63041cb
Himanshu Yadav <himanshuyadav@Himanshus-MacBook-Pro.local>
2024-11-21 18:15:39 +0530

    refactor: 💡 removed query dev tools

--- src/providers/app-provider.tsx
+++ src/providers/app-provider.tsx
@@ -4,7 +4,6 @@ import React, { useEffect } from 'react'
 import { SocketProvider } from '@/hooks/use-socket'
 import { updateUserData } from '@/store/global-store'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
@@ -42,7 +41,6 @@ const AppProvider = ({
 					>
 						<NextTopLoader />
 						{children}
-						<ReactQueryDevtools />
 					</ThemeProvider>
 				</QueryClientProvider>
 			</SocketProvider>
