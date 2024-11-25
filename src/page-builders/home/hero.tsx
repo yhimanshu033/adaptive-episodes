f248f5ba8c768a32c89ce865cc483890b9cb6a27
Himanshu Yadav <himanshuyadav@Himanshus-MacBook-Pro.local>
2024-11-25 08:40:41 +0530

    fix: 🐛 added api key

--- src/page-builders/home/hero.tsx
+++ src/page-builders/home/hero.tsx
@@ -13,6 +13,7 @@ const Hero = () => {
 				alt="logo_animation"
 				width={300}
 				height={300}
+				unoptimized
 			/>
 			<h1 className="font-display text-center text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
 				Welcome to Pocket CoPilot
