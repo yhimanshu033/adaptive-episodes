4bd4e240cbbb44708315f044ee684009224b020d
sawan.kumar <sawan.kumar@pocketfm.com>
2024-11-26 10:17:52 +0000

    fix: 🐛 merging missing fix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -37,7 +37,7 @@
       enable = true;
       previews = {
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          command = ["npm" "run" "preview" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
           manager = "web";
         };
       };
