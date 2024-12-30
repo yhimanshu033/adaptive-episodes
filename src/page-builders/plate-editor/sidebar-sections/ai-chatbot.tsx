bc140628286a4b9de197dce19789cbfcee502583
Varad Prabhu <varad.prabhu@pocketfm.com>
2024-12-30 16:55:41 +0530

    fix: 🐛 fixed copy paste

--- src/page-builders/plate-editor/sidebar-sections/ai-chatbot.tsx
+++ src/page-builders/plate-editor/sidebar-sections/ai-chatbot.tsx
@@ -373,7 +373,10 @@ const AIChatbot = () => {
 									<Button
 										onClick={() => {
 											void navigator.clipboard.writeText(
-												extractBetweenTags(message.content, 'text')
+												extractBetweenTags(
+													extractBetweenTags(message.content, 'answer'),
+													'text'
+												)
 											)
 										}}
 										variant="ghost"
