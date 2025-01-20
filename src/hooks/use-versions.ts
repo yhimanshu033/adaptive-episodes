b77ac31ac4f66c308e80ea46bae85ffc19bb7333
Varad Prabhu <varad.prabhu@pocketfm.com>
2025-01-20 13:53:11 +0530

    fix: 🐛 ep status aving

--- src/hooks/use-versions.ts
+++ src/hooks/use-versions.ts
@@ -2,15 +2,12 @@ import { useEffect, useMemo, useRef, useState } from 'react'
 import { useParams } from 'next/navigation'
 import { statuses } from '@/constants/episodes-constants'
 import useEpisodeHook from '@/hooks/mutation/use-episode-hook'
-import useComments from '@/hooks/plate/use-comments'
-import useEpisodeContent from '@/hooks/query/use-episode-content'
 import useCustomPlateStore from '@/store/plate-store'
 import { useQueryClient } from '@tanstack/react-query'
-import { useEditorPlugin, useEditorState } from '@udecode/plate-common/react'
+import { useEditorPlugin } from '@udecode/plate-common/react'
 
 import { useEpisodeContext } from '@/providers/episode-id-provider'
 import { FindReplacePlugin } from '@/lib/plate/plugins/find-replace'
-import { clearLasers } from '@/lib/utils/plate'
 
 import { BASE_STATUS, EStatus } from '@/types/common'
 import { ESidebar } from '@/types/plate-types'
@@ -37,9 +34,6 @@ export default function useVersions({
 	const { setViewMode } = useCustomPlateStore()
 	const { usePlateStoreContext } = useEpisodeContext()
 	const { sidebar } = usePlateStoreContext()
-	const { data } = useEpisodeContent()
-	const { children } = useEditorState()
-	const { allComments } = useComments()
 
 	const latestIndex = useMemo(
 		() => (latestStatus !== BASE_STATUS ? statuses.indexOf(latestStatus) : 0),
@@ -62,19 +56,7 @@ export default function useVersions({
 
 	const handleConfirm = async () => {
 		if (currentSelection.current) {
-			const text = JSON.stringify(clearLasers(children))
-			const chapterId = data?.chapter.parent
-			const comments = allComments
-			const prevProps = data?.chapter.props
 			await saveEpisodeMutation.mutateAsync({
-				status: data?.chapter.status || BASE_STATUS,
-				chapterId,
-				text,
-				comments,
-				prevProps,
-			})
-			await saveEpisodeMutation.mutateAsync({
-				chapterId,
 				text: 'Status update',
 				status: currentSelection.current,
 			})
@@ -85,6 +67,7 @@ export default function useVersions({
 			})
 		}
 	}
+
 	useEffect(() => {
 		setViewMode(
 			isChildEpisode ||
