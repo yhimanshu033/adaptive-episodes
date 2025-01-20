41cff15154ef13a40e969a4fc58c121049d93522
Varad Prabhu <varad.prabhu@pocketfm.com>
2025-01-20 11:02:21 +0530

    fix: 🐛 versions saving

--- src/hooks/use-versions.ts
+++ src/hooks/use-versions.ts
@@ -2,12 +2,15 @@ import { useEffect, useMemo, useRef, useState } from 'react'
 import { useParams } from 'next/navigation'
 import { statuses } from '@/constants/episodes-constants'
 import useEpisodeHook from '@/hooks/mutation/use-episode-hook'
+import useComments from '@/hooks/plate/use-comments'
+import useEpisodeContent from '@/hooks/query/use-episode-content'
 import useCustomPlateStore from '@/store/plate-store'
 import { useQueryClient } from '@tanstack/react-query'
-import { useEditorPlugin } from '@udecode/plate-common/react'
+import { useEditorPlugin, useEditorState } from '@udecode/plate-common/react'
 
 import { useEpisodeContext } from '@/providers/episode-id-provider'
 import { FindReplacePlugin } from '@/lib/plate/plugins/find-replace'
+import { clearLasers } from '@/lib/utils/plate'
 
 import { BASE_STATUS, EStatus } from '@/types/common'
 import { ESidebar } from '@/types/plate-types'
@@ -26,7 +29,6 @@ export default function useVersions({
 	const { id } = useParams()
 	const currentSelection = useRef<EStatus>()
 	const [isDialogOpen, setIsDialogOpen] = useState(false)
-
 	const queryClient = useQueryClient()
 
 	const { saveEpisodeMutation } = useEpisodeHook()
@@ -35,6 +37,9 @@ export default function useVersions({
 	const { setViewMode } = useCustomPlateStore()
 	const { usePlateStoreContext } = useEpisodeContext()
 	const { sidebar } = usePlateStoreContext()
+	const { data } = useEpisodeContent()
+	const { children } = useEditorState()
+	const { allComments } = useComments()
 
 	const latestIndex = useMemo(
 		() => (latestStatus !== BASE_STATUS ? statuses.indexOf(latestStatus) : 0),
@@ -55,14 +60,21 @@ export default function useVersions({
 		}
 	}
 
-	useEffect(() => {
-		setViewMode(sidebar === ESidebar.FAR && !!replaceEnabled)
-		// eslint-disable-next-line  react-hooks/exhaustive-deps
-	}, [replaceEnabled, sidebar])
-
 	const handleConfirm = async () => {
 		if (currentSelection.current) {
+			const text = JSON.stringify(clearLasers(children))
+			const chapterId = data?.chapter.parent
+			const comments = allComments
+			const prevProps = data?.chapter.props
 			await saveEpisodeMutation.mutateAsync({
+				status: data?.chapter.status || BASE_STATUS,
+				chapterId,
+				text,
+				comments,
+				prevProps,
+			})
+			await saveEpisodeMutation.mutateAsync({
+				chapterId,
 				text: 'Status update',
 				status: currentSelection.current,
 			})
@@ -75,10 +87,20 @@ export default function useVersions({
 	}
 	useEffect(() => {
 		setViewMode(
-			isChildEpisode || selectedIndex < latestIndex + Number(isChildEpisode)
+			isChildEpisode ||
+				selectedIndex < latestIndex + Number(isChildEpisode) ||
+				(sidebar === ESidebar.FAR && !!replaceEnabled)
 		)
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [isChildEpisode, latestIndex, latestStatus, selectedIndex, selectedStatus])
+	}, [
+		isChildEpisode,
+		latestIndex,
+		latestStatus,
+		selectedIndex,
+		selectedStatus,
+		sidebar,
+		replaceEnabled,
+	])
 
 	return {
 		handleConfirm,
