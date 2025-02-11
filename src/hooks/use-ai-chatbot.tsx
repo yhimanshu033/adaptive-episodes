8fc8eccaaddf47439315265b06a9576137fe29a4
Himanshu Yadav <himanshuyadav@Himanshus-MacBook-Pro.local>
2025-02-11 18:31:10 +0530

    refactor: 💡 changed info content
    BREAKING CHANGE: 🧨 no

--- src/hooks/use-ai-chatbot.tsx
+++ src/hooks/use-ai-chatbot.tsx
@@ -286,7 +286,7 @@ export function ChatbotProvider({
 					taskId: aiResponse,
 					role: EMessenger.ASSISTANT,
 					action: EAction.VOICE,
-					content: 'Erstelle VOICE Tags...',
+					content: 'Voice Pass ist aktiv...',
 				})
 			} else {
 				handleBlock({ text: '', taskId: aiResponse })
