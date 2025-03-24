cf570730d3f93008e5654a0e0f51a19c7ad6ce16
Varad Prabhu <varad.prabhu@pocketfm.com>
2025-03-24 17:16:48 +0530

    feat: 🎸 promo page init

--- src/constants/query-constants.ts
+++ src/constants/query-constants.ts
@@ -20,6 +20,7 @@ export const LASERTOOLS_QUERY_KEY = 'lasertools'
 export const METADATA_QUERY_KEY = 'metadata'
 export const PLOTOUTLINE_QUERY_KEY = 'plotoutline'
 
+export const TRANSLATE_VIDEO_MUTATION_KEY = 'translate-video'
 //Mutation keys
 
 export const NOTES_MUTATION = 'update-notes'
