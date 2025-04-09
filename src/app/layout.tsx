cd2b9ce8506aab849fa2723c716b0945ac0afd9d
Varad Prabhu <varad.prabhu@pocketfm.com>
2025-04-09 20:22:31 +0530

    feat: 🎸 i18n setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { fontSans } from '@/lib/fonts'
 
 import '@/styles/globals.css'
 
+import { NextIntlClientProvider } from 'next-intl'
+import { getLocale } from 'next-intl/server'
+
 import AppProvider from '@/providers/app-provider'
 import getServerSession from '@/lib/get-access-token'
 
@@ -20,10 +23,13 @@ export default async function RootLayout({
 	children: React.ReactNode
 }>) {
 	const session = await getServerSession()
+	const locale = await getLocale()
 	return (
-		<html lang="en" suppressHydrationWarning>
+		<html lang={locale} suppressHydrationWarning>
 			<body className={`${fontSans.className} antialiased`}>
-				<AppProvider session={session}>{children}</AppProvider>
+				<NextIntlClientProvider>
+					<AppProvider session={session}>{children}</AppProvider>
+				</NextIntlClientProvider>
 			</body>
 		</html>
 	)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Home from '@/page-builders/home'
+import { getTranslations } from 'next-intl/server'
 
-export default function Page() {
+export default async function Page() {
+	const t = await getTranslations('HomePage')
+	console.log({ d: t('title') })
 	return <Home />
 }
