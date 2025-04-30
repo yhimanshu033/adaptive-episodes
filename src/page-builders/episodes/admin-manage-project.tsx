a6bc58404275c7e08ac3b4106f2d95d13c063945
Varad Prabhu <varad.prabhu@pocketfm.com>
2025-04-30 13:47:06 +0530

    fix: 🐛 temp google id

--- src/page-builders/episodes/admin-manage-project.tsx
+++ src/page-builders/episodes/admin-manage-project.tsx
@@ -3,13 +3,15 @@ import Link from 'next/link'
 import { useParams } from 'next/navigation'
 import useIsGerman from '@/hooks/use-is-german'
 
+import useProjectId from '@/providers/project-id-provider'
 import { buttonVariants, cn } from '@/lib/utils/helpers'
 
 const AdminManageProject = () => {
 	const { id } = useParams()
 	const isGerman = useIsGerman()
+	const { isAdmin } = useProjectId()
 
-	if (!isGerman) {
+	if (!isGerman || !isAdmin) {
 		return null
 	}
 	return (
