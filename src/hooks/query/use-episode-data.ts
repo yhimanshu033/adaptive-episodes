f201dfb276516e572a8687292471e3fc97c2aa57
Varad <varad.prabhu@pocketfm.com>
2025-05-19 13:44:45 +0530

    Revert "Revert "Staging""

--- src/hooks/query/use-episode-data.ts
+++ src/hooks/query/use-episode-data.ts
@@ -5,6 +5,8 @@ import { EPISODE_LIST_QUERY_KEY } from '@/constants/query-constants'
 import { getEpisodes } from '@/server-action/episode-action'
 import { useQuery } from '@tanstack/react-query'
 
+import { addOpenedEpisodeList } from '@/lib/utils/indexed-db'
+
 export const useEpisodesData = (
 	title: string = '',
 	page: number = 1,
@@ -13,15 +15,31 @@ export const useEpisodesData = (
 	const { id } = useParams()
 	const storyId = Number(id)
 
+	async function getEpisodesData() {
+		const data = await getEpisodes({
+			project_id: storyId,
+			page,
+			search: title,
+			limit,
+		})
+
+		if (!data?.results.data.length) {
+			await addOpenedEpisodeList({
+				data: {
+					page: 1,
+					search: '',
+					seqNumber: undefined,
+				},
+				project: storyId,
+			})
+		}
+
+		return data
+	}
+
 	const query = useQuery({
 		queryKey: [EPISODE_LIST_QUERY_KEY, storyId, page, title, limit],
-		queryFn: () =>
-			getEpisodes({
-				project_id: storyId,
-				page,
-				search: title,
-				limit,
-			}),
+		queryFn: getEpisodesData,
 	})
 
 	return query
