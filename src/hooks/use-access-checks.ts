161a3ca44742523e7c490b2f14a9b05e46e1b7b8
Himanshu Yadav <himanshuyadav@Himanshus-MacBook-Pro.local>
2025-06-11 13:03:07 +0530

    fix: 🐛 auto refresh on tab change
    BREAKING CHANGE: 🧨 no

--- src/hooks/use-access-checks.ts
+++ src/hooks/use-access-checks.ts
@@ -1,16 +1,9 @@
-import { useMemo } from 'react'
-import { useSession } from 'next-auth/react'
-
 import useEpisodeTableContext from '@/providers/episode-table-provider'
-import { isInternalUser } from '@/lib/utils/helpers'
 
 import { ELanguage } from '@/types/common'
 
 const useAccessChecks = () => {
 	const { initialStoryData } = useEpisodeTableContext()
-	const { data: session } = useSession()
-
-	const isInternal = useMemo(() => isInternalUser(session), [session])
 
 	const isGerman = initialStoryData?.parent_language
 		? initialStoryData.parent_language === ELanguage.GERMAN_ORIGINAL
@@ -18,7 +11,7 @@ const useAccessChecks = () => {
 
 	const isOriginal = initialStoryData?.is_original || false
 
-	return { isInternal, isGerman, isOriginal }
+	return { isGerman, isOriginal }
 }
 
 export default useAccessChecks
