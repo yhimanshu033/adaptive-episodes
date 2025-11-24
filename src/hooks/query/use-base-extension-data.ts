299e72af407075c7c6bf6f8471fcfb0f6c032ea8
Himanshu Yadav <himanshuyadav@Himanshus-MacBook-Pro.local>
2025-11-24 17:12:25 +0530

    fix: 🐛 update bse info api
    BREAKING CHANGE: 🧨 no

--- src/hooks/query/use-base-extension-data.ts
+++ src/hooks/query/use-base-extension-data.ts
@@ -3,35 +3,25 @@ import { API_URLS } from '@/constants/global-constants'
 import { BASE_EXTENSION_QUERY_KEY } from '@/constants/query-constants'
 import { useQuery } from '@tanstack/react-query'
 
-import { FetchResponseResult } from '@/lib/fetch-api'
+import { fetchAPI } from '@/lib/fetch-api'
 
 import { TBaseScriptExtensionResponse } from '@/types/admin-types'
-import { TNoParams } from '@/types/common'
 
-import useSocket from '../use-socket'
 import useGDriveAuth from './use-gdrive-auth'
-
-type GetBaseResponse<T> = FetchResponseResult<T> | T
 
 const useBaseExtensionQuery = (enabled: boolean) => {
 	const { id } = useParams()
-	const { startTask, getResponse } = useSocket()
 	const { redirectToGDriveAuth } = useGDriveAuth()
 
 	const getBaseExtensionData = async () => {
-		const taskId = await startTask<
-			TNoParams,
+		const resp = await fetchAPI<
 			TBaseScriptExtensionResponse,
 			{ projectId: number }
 		>({
 			method: 'GET',
 			url: API_URLS.GET_BASE_SCRIPT_EXTENSION,
 			urlParams: { projectId: Number(id) },
-			noCache: true,
 		})
-
-		const resp =
-			await getResponse<GetBaseResponse<TBaseScriptExtensionResponse>>(taskId)
 
 		if ('status' in resp) {
 			if (resp.status === 401) {
