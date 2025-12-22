862b1f9aae4ee6f39c823853d2c0565a50581dd9
Himanshu Yadav <himanshuyadav@Himanshus-MacBook-Pro.local>
2025-12-22 19:36:59 +0530

    fix: 🐛 access fix
    BREAKING CHANGE: 🧨 no

--- src/hooks/use-recent-user.ts
+++ src/hooks/use-recent-user.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
+import useEpisodeIdStore from '@/store/episode-id-store'
 import { useSession } from 'next-auth/react'
-import { useEpisodeIdStore } from 'unified-editor'
 import { useShallow } from 'zustand/react/shallow'
 
 export default function useRecentUser() {
