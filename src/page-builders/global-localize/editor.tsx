1b43dc7fbbe0e0f89e08d2c5c21ffb27bf2dea6c
Himanshu Yadav <himanshuyadav@Himanshus-MacBook-Pro.local>
2025-12-23 13:01:58 +0530

    fix: 🐛 minor fixes
    BREAKING CHANGE: 🧨 no

--- src/page-builders/global-localize/editor.tsx
+++ src/page-builders/global-localize/editor.tsx
@@ -5,7 +5,7 @@ import { EpisodeIdProvider } from '@/providers/episode-id-provider'
 
 export default function Editor({ episodeId }: { episodeId: number }) {
 	return (
-		<main className="flex flex-1 flex-col">
+		<main className="flex h-[calc(100dvh-116px)] flex-1 flex-col">
 			<EpisodeIdProvider key={String(episodeId)} episodeId={Number(episodeId)}>
 				<UCE />
 			</EpisodeIdProvider>
